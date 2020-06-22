92be76d531e4b749102beaa12bae4682eeb2c1c4
cmdalo <camilodaloisio@gmail.com>
2020-06-22 20:07:21 -0300

    probando. en la carpeta terminal puse la impl. de acelerar

--- TPI/Terminal/reunionesRemotas/src/solucion.cpp
+++ TPI/Terminal/reunionesRemotas/src/solucion.cpp
@@ -22,7 +22,18 @@ bool esReunionValida(reunion r, int prof, int freq) {
 
 
 void acelerar(reunion& r, int prof, int freq) {
-    // Implementacions
+    reunion rV=r;
+    for(int i=0; i<rV.size(); i++){
+        int a=0;
+        for(int j=1; j<rV[i].first.size();j=j+2){
+            r[i].first[(j-1)/2]=rV[i].first[j];
+            a=(j+1)/2;
+        }
+        for(int q=0; q<rV[i].first.size()-a; q++){
+            r[i].first.pop_back();
+        }
+        r[i].second=rV[i].second;
+    }
     return;
 }
 
