6c5d1bffa6a902d620056f98401f9f4a8e632a90
cmdalo <camilodaloisio@gmail.com>
2020-06-27 15:12:00 -0300

    agregue test a reconstruir

--- TPI/CLion/reunionesRemotas/tests/reconstruir.cpp
+++ TPI/CLion/reunionesRemotas/tests/reconstruir.cpp
@@ -16,3 +16,30 @@ TEST(reconstruirTEST, reconstruirValidos){
 
     ASSERT_HABLANTES_EQ(hablanteObtenido, hablante1Reconstruido);
 }
+
+TEST(reconstruirTEST, reconstruirValidosConPasajePorCero){
+    senial hablante2 = {1, 3, 2, 3, -8, 0, 4, 0, 8, -15};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante2Reconstruido = {1, 3, 2, 3, -8, 0, 4, 6, 8, -15};
+
+    senial hablanteObtenido2 = reconstruir(hablante2, prof, freq);
+
+    ASSERT_HABLANTES_EQ(hablanteObtenido2, hablante2Reconstruido);
+}
+
+TEST(reconstruirTEST, reconstruirValidosConTresCeros){
+    senial hablante3 = {1, 3, 2, 3, -8, 0, 0, 0, 10, -15};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante3Reconstruido = {1, 3, 2, 3, -8, 1, 1, 1, 10, -15};
+
+    senial hablanteObtenido3 = reconstruir(hablante3, prof, freq);
+
+    ASSERT_HABLANTES_EQ(hablanteObtenido3, hablante3Reconstruido);
+}
+
+
+
