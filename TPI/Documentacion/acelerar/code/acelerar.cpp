32bd26c6587a075637063b07ab56d3b9dd17502b
cmdalo <camilodaloisio@gmail.com>
2020-07-01 21:08:00 -0300

    complejidad acelerar

---
+++ TPI/Documentacion/acelerar/code/acelerar.cpp
@@ -0,0 +1,14 @@
+void acelerar(reunion &r, int p, int f){ //llamo m=r.size() y n=r[0].size()
+    reunion rV=r;			
+    for(int i=0; i<rV.size(); i++){
+        int a=0;
+        for(int j=1; j<rV[i].first.size();j=j+2){
+            r[i].first[(j-1)/2]=rV[i].first[j];
+            a=(j+1)/2;
+        }
+        for(int q=0; q<rV[i].first.size()-a; q++){
+            r[i].first.pop_back();
+        }
+        r[i].second=rV[i].second;
+    }
+}
