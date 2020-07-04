1f8ef54de684a331ca1812ff18658a7ecb514048
NicoKc <nicolaskinaschuk@gmail.com>
2020-07-04 15:33:01 -0300

    Refactor de acelerar

--- TPI/CLion/reunionesRemotas/src/solucion.cpp
+++ TPI/CLion/reunionesRemotas/src/solucion.cpp
@@ -123,15 +123,16 @@ bool esReunionValida(reunion r, int prof, int freq) {
 void acelerar(reunion &r, int prof, int freq) {
     reunion rV = r;
     for (int i = 0; i < rV.size(); i++) {
-        int a = 0;
-        for (int j = 1; j < rV[i].first.size(); j = j + 2) {
+        int j = 1;
+        while (j < rV[i].first.size()) {
             r[i].first[(j - 1) / 2] = rV[i].first[j];
-            a = (j + 1) / 2;
+            j = j + 2;
         }
+
+        int a = j / 2;
         for (int q = 0; q < rV[i].first.size() - a; q++) {
             r[i].first.pop_back();
         }
-        r[i].second = rV[i].second;
     }
 }
 /*===========================================================================*/
