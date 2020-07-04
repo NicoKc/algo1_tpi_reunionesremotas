66b96f8af527bcf9f4da1afb83dea23d5c4dab36
NicoKc <nicolaskinaschuk@gmail.com>
2020-07-04 09:53:37 -0300

    Se actualiza el codigo de la documentación de acelerar

--- TPI/Documentacion/acelerar/code/acelerar.cpp
+++ TPI/Documentacion/acelerar/code/acelerar.cpp
@@ -1,14 +1,14 @@
-void acelerar(reunion &r, int p, int f){
-    reunion rV=r;			
-    for(int i=0; i<rV.size(); i++){
-        int a=0;
-        for(int j=1; j<rV[i].first.size();j=j+2){
-            r[i].first[(j-1)/2]=rV[i].first[j];
-            a=(j+1)/2;
+void acelerar(reunion &r, int prof, int freq) {
+    reunion rV = r;
+    for (int i = 0; i < rV.size(); i++) {
+        int a = 0;
+        for (int j = 1; j < rV[i].first.size(); j = j + 2) {
+            r[i].first[(j - 1) / 2] = rV[i].first[j];
+            a = (j + 1) / 2;
         }
-        for(int q=0; q<rV[i].first.size()-a; q++){
+        for (int q = 0; q < rV[i].first.size() - a; q++) {
             r[i].first.pop_back();
         }
-        r[i].second=rV[i].second;
+        r[i].second = rV[i].second;
     }
 }
