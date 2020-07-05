b9942c42ac1cff4beb7e4024b2cb83f4256f5481
NicoKc <nicolaskinaschuk@gmail.com>
2020-07-04 21:51:36 -0300

    Actualización de complejidad

--- TPI/Documentacion/hablantesSuperpuestos/code/hablantesSuperpuestos.cpp
+++ TPI/Documentacion/hablantesSuperpuestos/code/hablantesSuperpuestos.cpp
@@ -1,20 +1,26 @@
 bool hablantesSuperpuestos(reunion r, int prof, int freq, int umbral) {
     bool resp = false;
-    vector<int> hablando(r[0].first.size()); 
+    vector<bool> hablando(r[0].first.size(), false);
     int j = 0;
-    while(j < r.size() && !resp){
+    while (j < r.size() && !resp) {
         int i = 0;
-        while( i < r[0].first.size()-1 && !resp){
-            if(((abs(r[j].first[i])) >= umbral) || ((abs(r[j].first[i])) < umbral) && ((abs(r[j].first[i+1])) >= umbral)) {
-                if(hablando[i] == 1){
-                    resp=true;
-                }else{
-                    hablando[i] = 1;
+        int cont = 0;
+        while (i < r[0].first.size() && !resp) {
+            if (valorAbsoluto(r[j].first[i]) < umbral) {
+                cont++;
+            } else {
+                if (cont > 0) {
+                    if (cont < 2) {
+                        resp = hablando[i - 1];
+                        hablando[i - 1] = true;
+                    }
+                    cont = 0;
                 }
-                i++;
-            }else{
-                i++;
+
+                resp = resp || hablando[i];
+                hablando[i] = true;
             }
+            i++;
         }
         j++;
     }
---
+++ TPI/Documentacion/hablantesSuperpuestos/code/valorAbsoluto.cpp
@@ -0,0 +1,6 @@
+int valorAbsoluto(int &n){
+    if(n<0){
+        n = n * (-1);
+    }
+    return n;
+}
