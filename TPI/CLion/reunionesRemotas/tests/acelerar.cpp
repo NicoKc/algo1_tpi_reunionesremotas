5224c2029be0a61172e4e5c67a5702cda86b5afd
NicoKc <nicolaskinaschuk@gmail.com>
2020-07-05 10:11:37 -0300

    Corrección de documentación

--- TPI/CLion/reunionesRemotas/tests/acelerar.cpp
+++ TPI/CLion/reunionesRemotas/tests/acelerar.cpp
@@ -36,3 +36,19 @@ TEST(acelerarTEST, aceleracionValidaPar){
 
     ASSERT_REUNION_EQ(reunionOriginal, reunionAcelerada);
 }
+
+TEST(acelerarTEST, aceleracionValidaImpar2){
+    senial hablante1 = {1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 7, 1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 60};
+    senial hablante2 = {1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 7, 1, 3, -3, 4, 6, 0, -2, -8, 9, -15, 60};
+    reunion reunionOriginal = {make_pair(hablante1, 0), make_pair(hablante2, 1)};
+    int prof = 8;
+    int freq = 10;
+
+    senial hablante1Acelerado = {3, 4, 0, -8, -15, 1, -3, 6, -2, 9, 60};
+    senial hablante2Acelerado = {3, 4, 0, -8, -15, 1, -3, 6, -2, 9, 60};
+    reunion reunionAcelerada = {make_pair(hablante1Acelerado, 0), make_pair(hablante2Acelerado, 1)};
+
+    acelerar(reunionOriginal, prof, freq);
+
+    ASSERT_REUNION_EQ(reunionOriginal, reunionAcelerada);
+}
